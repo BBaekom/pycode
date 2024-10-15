2135305a46f3b869699f52cd501e5fa13ea1b18b
Lee Jongmin <ljm0813b@gmail.com>
2024-10-16 00:24:04 +0900

    05_Bright

---
+++ Computer_Vision/05_Bright.py
@@ -0,0 +1,22 @@
+import cv2
+
+def brightness4():
+    src = cv2.imread('cat.bmp', cv2.IMREAD_GRAYSCALE)
+
+    if src is None:
+        print('Image load failed!')
+        return
+
+    def update(pos):
+        dst = cv2.add(src, pos)
+        cv2.imshow('dst', dst)
+
+    cv2.namedWindow('dst')
+    cv2.createTrackbar('Brightness', 'dst', 0, 100, update)
+    update(0)
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+brightness4()
