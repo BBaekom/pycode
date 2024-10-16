25f73efc8753eb81566873f0a4cfa5a4de69ffaf
Lee Jongmin <ljm0813b@gmail.com>
2024-10-16 19:15:50 +0900

    Add Blurring_gaussian

--- Computer_Vision/07_Blurring.py
+++ Computer_Vision/07_Blurring.py
@@ -22,4 +22,26 @@ def blurring_mean():
     cv2.destroyAllWindows()
 
 
-blurring_mean()
+def blurring_gaussian():
+    src = cv2.imread('cat.bmp', cv2.IMREAD_GRAYSCALE)
+
+    if src is None:
+        print('Image load failed!')
+        return
+
+    cv2.imshow('src', src)
+
+    for sigma in range(1, 6):
+        dst = cv2.GaussianBlur(src, (0, 0), sigma)
+
+        desc = "Gaussian: sigma = %d" % (sigma)
+        cv2.putText(dst, desc, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+                    1.0, 255, 1, cv2.LINE_AA)
+
+        cv2.imshow('dst', dst)
+        cv2.waitKey()
+
+    cv2.destroyAllWindows()
+
+
+blurring_gaussian()
