87504b6a01270add29dff098b99af520f0eeab45
Lee Jongmin <ljm0813b@gmail.com>
2024-10-18 16:20:51 +0900

    canny_edge

--- Computer_Vision/09_Gradient.py
+++ Computer_Vision/09_Gradient.py
@@ -47,5 +47,23 @@ def sobel_edge():
     cv2.destroyAllWindows()
 
 
+def canny_edge():
+    src = cv2.imread('cat.bmp', cv2.IMREAD_GRAYSCALE)
+
+    if src is None:
+        print('Image load failed!')
+        return
+
+    dst1 = cv2.Canny(src, 50, 100)
+    dst2 = cv2.Canny(src, 50, 150)
+
+    cv2.imshow('src', src)
+    cv2.imshow('dst1', dst1)
+    cv2.imshow('dst2', dst2)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
 # sobel_derivative()
-sobel_edge()
+# sobel_edge()
+canny_edge()
